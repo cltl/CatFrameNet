496f39cb791820801d65079a0aa2406b5b1cf156
piekvossen <piek.vossen@vu.nl>
2016-11-11 17:38:15 +0100

    -- deployment

--- matchFrameNetFrames.sh
+++ matchFrameNetFrames.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 LIB=target/CatFrameNet-1.0-SNAPSHOT-jar-with-dependencies.jar
-FRAMENET=vua-resources/frRelation.xml
+FRAMENET=../vua-resources/frRelation.xml
 INPUT=annotated_files
 EXTENSION=".xml"
 
-java  -Xmx4000m -cp $LIB MatchCatFrameNetFiles --folder $INPUT --extension $EXTENSION--framenet $FRAMENET
+java  -Xmx4000m -cp $LIB MatchCatFrameNetFiles --folder $INPUT --extension $EXTENSION --framenet $FRAMENET
